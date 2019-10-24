430180ed30e1f0abf2b5d997a8ec0b94f4270dca
Michael Loyselle <52831149+Leozas@users.noreply.github.com>
2019-10-24 13:43:44 -0400

    update randomnavs

--- reactplayground/src/App.js
+++ reactplayground/src/App.js
@@ -12,13 +12,83 @@ function Grid() {
 
 function App() {
   return (
-    <div className="row border">
-      <div className="col-4 border btn-primary">
-        <p>test</p>
-        <p>testing2</p>
-      </div>
-    </div>
-  );
-}
 
-export default App;
+    <nav className="navbar navbar-expand-lg navbar-primary bg-light fixed-top">
+      <button className="navbar-toggler bg-dark" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle Navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <a className="nav-link" href="https://www.google.com">Google</a></li>
+          <li className="nav-item border"> <a className="nav-link" href="https://www.github.com">GitHub</a></li>
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>       
+              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a className="dropdown-item" href="#">Action</a>
+                <a className="dropdown-item" href="#">Another action</a>
+                <div className="dropdown-divider"></div>
+                <a className="dropdown-item" href="#">Something else here</a>
+              </div>
+            </li>
+        </ul>
+
+      </div>
+    </nav>
+
+      );
+    }
+    
+    
+    
+    export default App;
+    
+    
+    /*
+    
+    <div className="row border">
+        <div className="col-4 border btn-primary">
+          <button type="button" class="btn btn-primary">Primary</button>
+          <button type="button" class="btn btn-primary">Primary</button>
+          <button type="button" class="btn btn-primary">Primary</button>
+          <button type="button" class="btn btn-primary">Primary</button>
+        </div>
+      </div>
+
+
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Dropdown
+        </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Something else here</a>
+              </div>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            </li>
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+        */
---
+++ reactplayground/src/components/navbar-top.js
@@ -0,0 +1,12 @@
+<nav className="navbar navbar-expand-lg navbar-primary bg-primary">
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle Navigation">
+        <span className="navbar-toggler-icon"></span>
+    </button> 
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+                <a className="nav-link" href="https://www.google.com">Google</a>
+            </li>
+        </ul>
+    </div>
+</nav>
